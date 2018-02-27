69a3875ad175c0ed10b76b62a268558a355260f7
acordafrederick <sonjaire@gmail.com>
2018-02-27 17:09:58 -0600

    work flow's complete. logic in progress

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+	{
+		"name": "Frederick Acorda",
+		"photo": "https://goo.gl/images/QbggBZ",
+		"scores": [
+			"3",
+			"3",
+			"2",
+			"1",
+			"4",
+			"1",
+			"4",
+			"2",
+			"4",
+			"4"
+		]
+	}
+
+];
+
+module.exports = friendsArray;
